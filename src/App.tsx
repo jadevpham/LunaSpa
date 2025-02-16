5d0a0d775493ec1dababaeb68a4446a1b49949be
jadevpham <jadepham2101@gmail.com>
2025-02-17 04:02:46 +0700

    create redux, create template card branches

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Footer from "./components/Footer";
 import AuthPage from "./pages/AuthPage";
 import Header from "./components/Header";
+import BranchesList from "./components/BranchesList";
+
+// Cấu hình Redux
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 const App = () => {
 	const FooterWrapper = () => {
@@ -15,17 +19,25 @@ const App = () => {
 		return !location.pathname.includes("/auth") && <Header />;
 	};
 
+	const BranchesWrapper = () => {
+		const location = useLocation();
+		return !location.pathname.includes("/auth") && <BranchesList />;
+	};
+
 	return (
 		<BrowserRouter>
-			<HeaderWrapper />
-			<Routes>
-				<Route
-					path="/"
-					element={<h1 className="text-red-500">Hello, React!</h1>}
-				/>
-				<Route path="/auth/*" element={<AuthPage />} />
-			</Routes>
-			<FooterWrapper />
+			<Provider store={store}>
+				<HeaderWrapper />
+				<BranchesWrapper />
+				<Routes>
+					<Route
+						path="/"
+						element={<h1 className="text-red-500">Hello, React!</h1>}
+					/>
+					<Route path="/auth/*" element={<AuthPage />} />
+				</Routes>
+				<FooterWrapper />
+			</Provider>
 		</BrowserRouter>
 	);
 };
