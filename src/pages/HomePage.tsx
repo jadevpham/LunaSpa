78c7aefb81435e0ecf5bd43b9fc86d7a32dda1a8
jade <70644728+jadevpham@users.noreply.github.com>
2025-02-25 14:08:13 +0700

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,16 +1,9 @@
 import BranchesList from "../components/BranchesList";
 import Footer from "../components/Footer";
-<<<<<<< HEAD:src/pages/Home.tsx
 import Header from "../components/Header";
-import {
-	default as ProductsList,
-	default as ServicesList,
-} from "../components/ServicesList";
-=======
 import ServicesList from "../components/ServicesList";
 import BranchesList from "../components/BranchesList";
 import ProductsList from "../components/ProductsList";
->>>>>>> main:src/pages/HomePage.tsx
 import Statistics from "../components/Statistics";
 const HomePage = () => {
 	return (
