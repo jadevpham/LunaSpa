d74d22e85d262be4ebc47344ee1f62f649407a89
jadevpham <jadepham2101@gmail.com>
2025-03-28 12:51:19 +0700

    fix output2

--- src/redux/productsByServiceSlice.tsx
+++ src/redux/productsByServiceSlice.tsx
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
 import axios from "axios";
-//
+//.s
 type ProductsByServiceItemType = {
 	_id: string;
 	service_id: string;
